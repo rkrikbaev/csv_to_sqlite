49ccc043217df4215bcd36ce051c0c798195af88
rkrikbaev <rkrikbaev@gmail.com>
2023-06-18 15:25:23 +0600

    Update app.py
    add update_db function

--- app.py
+++ app.py
@@ -4,12 +4,26 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = 'secret_key'  # Replace with your secret key
 
-DATABASE = 'data.db'
+from config import config
+from parce_csv import *
+
+DATABASE = config.get('db_file')
+OBJECTS = config.get('objects')
+DIR = config.get('path_to_csv')
 
 def get_db_connection():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
+
+def update_db(object_name):
+    csv_files = find_csv_files(DIR)
+    for file in csv_files:
+        print(file)
+        to_sqlite(
+            data=from_csv(file), 
+            db_file=DATABASE, 
+            table_name=object_name)  
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -32,11 +46,12 @@ def data():
     if not session.get('logged_in'):
         return redirect('/')
     
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM data_table')
-    rows = cursor.fetchall()
-    conn.close()
+    update_db(directory)    
+#     conn = get_db_connection()
+#     cursor = conn.cursor()
+#     cursor.execute('SELECT * FROM data_table')
+#     rows = cursor.fetchall()
+#     conn.close()
     
     return render_template('data.html', rows=rows)
 
