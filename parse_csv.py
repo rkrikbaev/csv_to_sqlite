d98f79f8d8b8585a77682b3bd87d4420641e90f2
rkrikbaev <rkrikbaev@gmail.com>
2023-06-18 10:46:47 +0600

    Update parse_csv.py
    add check if same record exist

--- parse_csv.py
+++ parse_csv.py
@@ -8,8 +8,8 @@ import fnmatch
 
 from config import config
 
-db_file = config.get('db_csv.db')
-table_name = config.get('csv_data')
+db_file = config.get('db_file')
+table_name = config.get('table_name')
 directory = config.get('path_csv')
 
 def send_data(directory):
@@ -60,21 +60,28 @@ def csv_to_sqlite(csv_file, db_file='db_csv.db', table_name='csv_data'):
         # 2. create database
         connection=sqlite3.connect(db_file)
         curosr=connection.cursor()
+        
         # 3. execute table query to create table
         curosr.execute(Table_Query)
 
         # 4. pase csv data
         for row in csv_file_reader:
-            # skip the first row
-            for i in range(len(row)):
-                # assign each field its value
-                sr=row[0]
-                period=row[1]
-                data_value=row[2]
-                suppressed=row[3]
-                status = row[4]
-                units= row[5]
-                magnitude = row[6]
+                # skip the first row
+                for i in range(len(row)):
+                    # Check if the current value already exists in the table
+                    value_to_check = row[0]  # The value to check is in the first column
+                    check_query = f'SELECT COUNT(*) FROM {table_name} WHERE column_name = ?'  # Replace column_name with the appropriate column name
+                    cursor.execute(check_query, (value_to_check,))
+                    result = cursor.fetchone()
+                    if result[0] == 0:  # If the count is 0, the value doesn't exist in the table                    
+                        # assign each field its value
+                        sr=row[0]
+                        period=row[1]
+                        data_value=row[2]
+                        suppressed=row[3]
+                        status = row[4]
+                        units= row[5]
+                        magnitude = row[6]
 
             # 5. create insert query
             InsertQuery=f"INSERT INTO {table_name} VALUES ('{sr}','{period}','{data_value}','{suppressed}','{status}','{units}','{magnitude}')"
